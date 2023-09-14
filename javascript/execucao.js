e493549dfb8fae6785abb86e7f0ea58804aff04f
fernandobrit <128629919+fernandobrit@users.noreply.github.com>
2023-09-14 09:48:46 -0300

    teste de execução

--- javascript/execucao.js
+++ javascript/execucao.js
@@ -1,7 +1,7 @@
 function search_animal() {
     let input = document.getElementById('searchbar').value
     input=input.toLowerCase();
-    let x = document.getElementsByClassName('animals');
+    let x = document.getElementsByClassName('Produtos');
       
     for (i = 0; i < x.length; i++) { 
         if (!x[i].innerHTML.toLowerCase().includes(input)) {
